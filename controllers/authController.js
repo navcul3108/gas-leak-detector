7bb0be67cf724b25f7ca3fe46deaaffbc83356b8
Khang <hoanggiakhangsk@gmail.com>
2021-05-09 10:59:50 +0700

    add dotenv

---
+++ controllers/authController.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user.id);
+  const cookieOption = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOption.secure = true;
+
+  user.password = undefined;
+
+  res.cookie("jwt", token, cookieOption);
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  console.log(process.env.JWT_SECRET);
+  const newUser = await User.add(req.body);
+  console.log(newUser.id);
+
+  createSendToken(newUser, 201, res);
+});
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ const authController = require("../controllers/authController");
 /* GET users listing. */
 
 router.post("/signup", authController.signup);
-router.post("/login", authController.login);
+// router.post("/login", authController.login);
 
-router.get("/", authController.protect, userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 
 module.exports = router;
