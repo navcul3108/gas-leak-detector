81e48e4858482ed276fd4f44d2c1c5fed45cb4f5
Khang <hoanggiakhangsk@gmail.com>
2021-05-27 10:29:37 +0700

    api for mobile app

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // catch 404 and forward to error handler
+app.use("/api/user", usersRouter);
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 app.use(function(req, res, next) {
   next(createError(404));
